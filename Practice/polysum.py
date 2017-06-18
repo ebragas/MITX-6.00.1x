4557a278f4bfce7372b3820c71f615da52a294c7
Eric Bragas <ebragas@designmind.com>
2017-06-18 10:10:16 -0700

    Init polysum; calculates area and perimiter of regular polygon

---
+++ Practice/polysum.py
@@ -0,0 +1,31 @@
+
+# Polysum
+
+# Write a function that accepts 2 arguments, n (number of sides on a polygon)
+# and s (length of each side) and returns the sum of the polygon's area ad
+# perimeter squared.
+
+# A regular polygon is one that is equiangular and equilateral
+
+# https://courses.edx.org/courses/course-v1:MITx+6.00.1x+2T2017/courseware/0de4fecc5a9a4749923133fcf4de181f/344d02eadf134621a1f17f473ef14514/
+
+from math import *
+
+def main():
+    print(pi)
+    print(area(3, 5))
+
+
+def area(n, s):
+    """
+    Input: int n, number of sides of a regular polygon; int s, length of each side
+    Returns the area of the polygon.
+    """
+
+
+def perimeter():
+    pass
+
+
+if __name__ == '__main__':
+    main()
