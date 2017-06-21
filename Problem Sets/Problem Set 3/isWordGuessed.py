22c10fa5033a61c25e23bec9fd0d4e04860617d9
Eric Bragas <ebragas@designmind.com>
2017-06-20 17:38:24 -0700

    Implement function with with test cases

--- Problem Sets/Problem Set 3/isWordGuessed.py
+++ Problem Sets/Problem Set 3/isWordGuessed.py
@@ -1,12 +1,31 @@
 
-def isWordGuessed(secretWord, lettersGuessed):
-    """ 
+def isWordGuessed(SecretWord, LettersGuessed):
+    """
     secretWord: string, the word the user is guessing
     lettersGuessed: list, what letters have been guessed so far
     returns: boolean, True if all the letters of secretWord are in lettersGuessed;
         False otherwise
     """
-    return True
+    letters = len(SecretWord)
+    counter = 0
 
-# Function testing
-print(isWordGuessed('eric', ['e', 'r', 'i', 'c']))
+    for letter in LettersGuessed:
+        if letter in SecretWord:
+            counter += 1  # count letter
+        else:
+            pass  # don't count
+
+    if letters == counter:
+        return True
+    else:
+        return False
+
+
+
+# Test Cases
+SecretWord = 'apple'
+LettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
+# SecretWord = 'eric'
+# LettersGuessed = ['e', 'r', 'i', 'c']
+
+print(isWordGuessed(SecretWord, LettersGuessed))
